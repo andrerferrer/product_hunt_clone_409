ab808971d5cc689ae6d92a6a3c94e49e0975cd22
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-12 13:41:20 -0300

    Implement #full_name for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    if last_name
+      "#{first_name.capitalize} #{last_name.capitalize}"
+    else
+      first_name.capitalize
+    end
+  end
 end
